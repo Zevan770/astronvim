333c449bfa792e9d3f34798673c83c4c6536965a
赵泽文(Zhao Zewen) <151506788+hw770@users.noreply.github.com>
2024-08-19 21:36:16 +0800

    clipboard

--- lua/polish.lua
+++ lua/polish.lua
@@ -23,3 +23,16 @@ vim.filetype.add {
     [".*keybindings.*%.json"] = "jsonc",
   },
 }
+
+vim.g.clipboard = {
+  name = "win32yank-wsl",
+  copy = {
+    ["+"] = "win32yank.exe -i --crlf",
+    ["*"] = "win32yank.exe -i --crlf",
+  },
+  paste = {
+    ["+"] = "win32yank.exe -o --lf",
+    ["*"] = "win32yank.exe -o --lf",
+  },
+  cache_enabled = true,
+}
