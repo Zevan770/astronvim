ed27665836eda9efc55c97faac81eaa0cb4eb650
赵泽文(Zhao Zewen) <151506788+hw770@users.noreply.github.com>
2024-09-11 20:52:26 +0800

    fire

--- lua/plugins/fire-nvim.lua
+++ lua/plugins/fire-nvim.lua
@@ -1,3 +1,13 @@
+if true then return {} end
+if vim.g.started_by_firenvim then
+  vim.api.nvim_create_autocmd("VimEnter", {
+    callback = function()
+      vim.opt.showtabline = 0
+      vim.opt.laststatus = 0
+    end,
+  })
+end
+
 ---@type LazySpec
 return {
   {
@@ -8,19 +18,36 @@ return {
     lazy = not vim.g.started_by_firenvim,
     module = false,
     build = function() vim.fn["firenvim#install"](0) end,
-  },
-  -- disable these plugin when started by firenvim
-  -- { "folke/noice.nvim", optional = true, cond = not vim.g.started_by_firenvim },
-  {
-    "rebelot/heirline.nvim",
-    optional = true,
-    enable = not vim.g.started_by_firenvim,
-  },
-
-  {
-    "wallpants/ghost-text.nvim",
-    opts = {
-      -- config goes here
+    dependencies = {
+      -- disable these plugin when started by firenvim
+      -- { "folke/noice.nvim", optional = true, cond = not vim.g.started_by_firenvim },
+      {
+        "rebelot/heirline.nvim",
+        optional = true,
+        cond = not vim.g.started_by_firenvim,
+      },
+      {
+        "wallpants/ghost-text.nvim",
+        opts = {
+          -- config goes here
+        },
+        cond = not vim.g.started_by_firenvim,
+        optional = true,
+      },
     },
+    opts = function(self, opts)
+      vim.g.firenvim_config = {
+        globalSettings = { alt = "all" },
+        localSettings = {
+          [".*"] = {
+            cmdline = "neovim",
+            --content  = "text",
+            --priority = 0,
+            selector = "textarea",
+            takeover = "never",
+          },
+        },
+      }
+    end,
   },
 }
