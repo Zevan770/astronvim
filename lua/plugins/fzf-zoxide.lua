80e7cda039c86bf4220ddeeb084be09c1abc9425
赵泽文(Zhao Zewen) <151506788+hw770@users.noreply.github.com>
2024-09-11 20:53:57 +0800

    fzf-zoxide

---
+++ lua/plugins/fzf-zoxide.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "calebfroese/fzf-lua-zoxide",
+    dependencies = { "ibhagwan/fzf-lua" },
+    config = function() require("fzf-lua-zoxide").setup() end,
+    opts = function()
+      vim.keymap.set("n", "<leader>fz", function()
+        require("fzf-lua-zoxide").open {
+          ---@field preview? string The command to run in the preview window
+          preview = "less -R {}",
+          ---@field callback? fun(selected: string) Callback to run on select
+          callback = function(selected)
+            -- For example, you could open the directory in netrw after selecting one
+            -- vim.cmd("e " .. selected)
+          end,
+        }
+      end, { desc = "Fzf Dirs" })
+    end,
+  },
+}
