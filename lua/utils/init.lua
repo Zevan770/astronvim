e8d05d90ad954a94bcca8586a85edf736c7d41d1
赵泽文(Zhao Zewen) <151506788+hw770@users.noreply.github.com>
2024-12-30 05:34:56 +0800

    chores

--- lua/utils/init.lua
+++
@@ -1,5 +0,0 @@
-function MyFunction()
-  -- 这里是你的代码
-  print "Operator-pending mode activated"
-end
-vim.api.nvim_set_keymap("o", "zf", ":<C-U>lua MyFunction()<CR>", { noremap = true, silent = true })
