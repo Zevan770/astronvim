71d12803584a5422d5484e24894ba4f41d974faa
赵泽文(Zhao Zewen) <151506788+hw770@users.noreply.github.com>
2025-02-24 13:48:47 +0800

    lazydev

--- lua/plugins/languages/lua.lua
+++ lua/plugins/languages/lua.lua
@@ -3,6 +3,28 @@ return {
   {
     "AstroNvim/astrocommunity",
     { import = "astrocommunity.pack.lua" },
-    { import = "astrocommunity.neovim-lua-development.lazydev-nvim" },
+  },
+  {
+    "folke/lazydev.nvim",
+    ft = "lua",
+    cmd = "LazyDev",
+    opts = function(_, opts)
+      local utils = require "astrocore"
+      return utils.extend_tbl(opts, {
+        library = {
+          -- It can also be a table with trigger words / mods
+          -- Only load luvit types when the `vim.uv` word is found
+          -- { path = "luvit-meta/library", words = { "vim%.uv" } },
+          -- Load the wezterm types when the `wezterm` module is required
+          -- Needs `justinsgithub/wezterm-types` to be installed
+          { path = "wezterm-types", mods = { "wezterm" } },
+          -- "lazy.nvim",
+        },
+      })
+    end,
+  },
+  {
+    "https://github.com/nekowinston/wezterm-types",
+    dev = true,
   },
 }
