0ef2b1f70a657a8543f94133b26aabc8bb91dc3c
赵泽文(Zhao Zewen) <151506788+hw770@users.noreply.github.com>
2025-03-15 04:41:47 +0800

    markview render-markdown

--- lua/plugins/languages/markdown.lua
+++ lua/plugins/languages/markdown.lua
@@ -10,62 +10,89 @@ return {
   -- { import = "astrocommunity.note-taking.obsidian-nvim" },
 
   -- #region render-markdown.nvim
-  -- { import = "astrocommunity.markdown-and-latex.render-markdown-nvim" },
-  -- {
-  --   "MeanderingProgrammer/render-markdown.nvim",
-  --   opts = { file_types = { "markdown", "Avante", "copilot-chat", "codecompanion" } },
-  --   dependencies = {
-  --     "nvim-cmp",
-  --     opts = function(_, opts)
-  --       local cmp = require "cmp"
-  --       cmp.setup {
-  --         sources = cmp.config.sources {
-  --           { name = "render-markdown" },
-  --         },
-  --       }
-  --     end,
-  --   },
-  -- },
-  -- #endregion
-
-  -- { import = "astrocommunity.markdown-and-latex.markview-nvim" },
+  { import = "astrocommunity.markdown-and-latex.render-markdown-nvim" },
   {
-    "OXY2DEV/markview.nvim",
-    lazy = false,
+    "MeanderingProgrammer/render-markdown.nvim",
+    opts = { file_types = { "markdown", "Avante", "copilot-chat", "codecompanion" } },
     dependencies = {
-      "nvim-treesitter/nvim-treesitter",
+      "nvim-cmp",
       opts = function(_, opts)
-        if opts.ensure_installed ~= "all" then
-          opts.ensure_installed =
-            require("astrocore").list_insert_unique(opts.ensure_installed, { "html", "markdown", "markdown_inline" })
-        end
+        local cmp = require "cmp"
+        cmp.setup {
+          sources = cmp.config.sources {
+            { name = "render-markdown" },
+          },
+        }
       end,
     },
-    ---@type mkv.config
-    opts = {
-      preview = {
-        hybrid_modes = { "n" },
-        enable_hybrid_mode = true,
-        modes = { "n", "c" },
-        linewise_hybrid_mode = true,
-
-        callbacks = {
-          on_enable = function(_, win)
-            -- This will prevent Tree-sitter concealment being disabled on the cmdline mode
-            vim.wo[win].concealcursor = "c"
-          end,
-        },
-        icon_provider = "devicons",
-        filetypes = { "markdown", "quarto", "rmd", "html", "yaml", "codecompanion", "copilot" },
-      },
-    },
-    config = function(_, opts)
-      require("markview.extras.editor").setup()
-      require("markview.extras.headings").setup()
-      require("markview.extras.checkboxes").setup()
-      require("markview").setup(opts)
-    end,
   },
+  -- #endregion
+
+  -- {
+  --   "OXY2DEV/markview.nvim",
+  --   ft = function()
+  --     local plugin = require("lazy.core.config").spec.plugins["markview.nvim"]
+  --     local opts = require("lazy.core.plugin").values(plugin, "opts", false)
+  --     return opts.filetypes or { "markdown", "quarto", "rmd" }
+  --   end,
+  --   dependencies = {
+  --     "nvim-treesitter/nvim-treesitter",
+  --     opts = function(_, opts)
+  --       if opts.ensure_installed ~= "all" then
+  --         opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+  --           "html",
+  --           "markdown",
+  --           "markdown_inline",
+  --         })
+  --       end
+  --     end,
+  --   },
+  --   ---@type mkv.config
+  --   opts = {
+  --     preview = {
+  --       hybrid_modes = { "n" },
+  --       enable_hybrid_mode = true,
+  --       modes = { "n", "c" },
+  --       linewise_hybrid_mode = true,
+  --
+  --       callbacks = {
+  --         on_enable = function(_, win)
+  --           -- This will prevent Tree-sitter concealment being disabled on the cmdline mode
+  --           vim.wo[win].concealcursor = "c"
+  --           vim.wo[win].conceallevel = 2
+  --         end,
+  --       },
+  --       icon_provider = "devicons",
+  --       filetypes = {
+  --         "avante",
+  --         "markdown",
+  --         "quarto",
+  --         "rmd",
+  --         "html",
+  --         "yaml",
+  --         "codecompanion",
+  --         "copilot",
+  --       },
+  --     },
+  --     ---@diagnostic disable
+  --     markdown = {
+  --       list_items = {
+  --         wrap = true,
+  --         indent_size = function(buffer, item) end,
+  --       },
+  --       code_blocks = {
+  --         style = "simple"
+  --       }
+  --     },
+  --     ---@diagnostic enable
+  --   },
+  --   config = function(_, opts)
+  --     require("markview.extras.editor").setup()
+  --     require("markview.extras.headings").setup()
+  --     require("markview.extras.checkboxes").setup()
+  --     require("markview").setup(opts)
+  --   end,
+  -- },
 
   {
     "AstroNvim/astrocore",
