3b45059668c5ed72e11ed06007663e5d5b7db7ba
赵泽文(Zhao Zewen) <151506788+hw770@users.noreply.github.com>
2025-03-15 04:41:29 +0800

    conform

--- lua/plugins/lsp/conform.lua
+++ lua/plugins/lsp/conform.lua
@@ -6,7 +6,7 @@ return {
   {
     "stevearc/conform.nvim",
     ---@module "conform"
-    ---@param opts conform.setupOpts
+    ---@return conform.setupOpts
     opts = function(_, opts)
       -- opts.log_level = vim.log.levels.TRACE
 
@@ -21,16 +21,25 @@ return {
         end
         return select(1, ...)
       end
-      opts.formatters_by_ft = opts.formatters_by_ft or {}
-      opts.formatters_by_ft.markdown = function(bufnr) return { first(bufnr, "prettierd", "prettier"), "injected" } end
-      -- opts.formatters = {
-      --   injected = {
-      --     options = {},
-      --   },
-      -- }
+
       -- vim.notify(vim.inspect(opts), vim.log.levels.INFO, { title = "conform opts" })
-      return opts
+      return require("astrocore").extend_tbl(opts, {
+        formatters_by_ft = {
+          markdown = function(bufnr) return { first(bufnr, "prettierd", "prettier"), "injected" } end,
+        },
+        formatters = {
+          injected = { options = { ignore_errors = true } },
+        },
+      })
     end,
+    keys = {
+      {
+        "<leader>lF",
+        function() require("conform").format { formatters = { "injected" }, timeout_ms = 3000 } end,
+        mode = { "n", "v" },
+        desc = "Format Injected Langs",
+      },
+    },
     -- opts = {
     --   log_level = vim.log.levels.DEBUG,
     --   formatters_by_ft = {
