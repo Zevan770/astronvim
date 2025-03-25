e4a3f0b4e6dfba2d82f04310e32a3ee5090f200c
赵泽文(Zhao Zewen) <151506788+hw770@users.noreply.github.com>
2025-03-25 15:03:12 +0800

    astro v5 stuff

--- lua/plugins/astro_v5.lua
+++ lua/plugins/astro_v5.lua
@@ -1,5 +1,10 @@
--- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+if vim.g.vscode == 1 then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 return {
+  {
+    "AstroNvim/AstroNvim",
+    branch = "v5",
+    version = false,
+  },
   { "AstroNvim/astrocore", version = false, branch = "v2" },
   { "AstroNvim/astrolsp", version = false, branch = "v3" },
   { "AstroNvim/astroui", version = false, branch = "v3" },
