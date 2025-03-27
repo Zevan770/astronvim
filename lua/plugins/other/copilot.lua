b7f3095220a8fdb40db77f660d9dbb1e285cef83
赵泽文(Zhao Zewen) <151506788+hw770@users.noreply.github.com>
2025-03-28 01:59:43 +0800

    blink-copilot

--- lua/plugins/other/copilot.lua
+++ lua/plugins/other/copilot.lua
@@ -3,15 +3,39 @@ return {
   {
     "AstroNvim/astrocommunity",
     -- { import = "astrocommunity.completion.copilot-lua" },
-    { import = "astrocommunity.completion.copilot-lua" },
+    -- { import = "astrocommunity.completion.copilot-cmp" },
     -- { import = "astrocommunity.completion.copilot-lua-cmp" },
   },
   {
     "zbirenbaum/copilot.lua",
-    config = function(_, opts)
-      vim.g.copilot_proxy_strict_ssl = false
-      require("copilot").setup { opts }
-    end,
+    cmd = "Copilot",
+    event = "InsertEnter",
+    opts = {
+      suggestion = { enabled = false },
+      panel = { enabled = false },
+      filetypes = {
+        markdown = true,
+        help = true,
+      },
+    },
+  },
+  {
+    "saghen/blink.cmp",
+    optional = true,
+    dependencies = { "fang2hou/blink-copilot" },
+    opts = {
+      sources = {
+        default = { "copilot" },
+        providers = {
+          copilot = {
+            name = "copilot",
+            module = "blink-copilot",
+            score_offset = 100,
+            async = true,
+          },
+        },
+      },
+    },
   },
   -- { import = "astrocommunity.completion.avante-nvim" },
   -- { import = "astrocommunity.editing-support.copilotchat-nvim" },
