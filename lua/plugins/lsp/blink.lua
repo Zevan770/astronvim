a53686f6edb3916b4ddac16c816e0b8f0a60e646
赵泽文(Zhao Zewen) <151506788+hw770@users.noreply.github.com>
2025-03-28 01:56:23 +0800

    blink keymap

--- lua/plugins/lsp/blink.lua
+++ lua/plugins/lsp/blink.lua
@@ -1,10 +1,16 @@
 return {
   "Saghen/blink.cmp",
-  opts = {
-    completion = {
+  ---@module 'blink.cmp'
+  ---@type blink.cmp.Config
+  opts = function(_, opts)
+    opts.keymap = {
+      preset = "enter",
+      ["<C-y>"] = { "select_and_accept" },
+    }
+    opts.completion = {
       menu = {
         auto_show = true,
       },
-    },
-  },
+    }
+  end,
 }
