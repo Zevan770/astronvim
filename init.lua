3cae9bd74347ad59c75c87ad3ef053b9e1cd475a
赵泽文(Zhao Zewen) <151506788+hw770@users.noreply.github.com>
2025-03-27 20:25:44 +0800

    suppress deprecated warning

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
+vim.deprecate = function() end
 require "snacks_profiler_setup"
 -- This file simply bootstraps the installation of Lazy.nvim and then calls other files for execution
 -- This file doesn't necessarily need to be touched, BE CAUTIOUS editing this file and proceed at your own risk.
