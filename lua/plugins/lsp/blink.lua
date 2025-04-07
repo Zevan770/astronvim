a18f982fad7ed0186fa2156af1cef03a847f342e
赵泽文(Zhao Zewen) <151506788+hw770@users.noreply.github.com>
2025-04-08 05:34:45 +0800

    add blink-ripgrep

--- lua/plugins/lsp/blink.lua
+++ lua/plugins/lsp/blink.lua
@@ -23,4 +23,60 @@ return {
     --   },
     -- }
   end,
+
+  {
+    "saghen/blink.cmp",
+    dependencies = { "mikavilpas/blink-ripgrep.nvim" },
+    opts = {
+      sources = {
+        default = { "ripgrep" },
+        providers = {
+          -- 👇🏻👇🏻 add the ripgrep provider config below
+          ripgrep = {
+            module = "blink-ripgrep",
+            name = "Ripgrep",
+            -- the options below are optional, some default values are shown
+            ---@module "blink-ripgrep"
+            ---@type blink-ripgrep.Options
+            opts = {
+              prefix_min_len = 3,
+              context_size = 5,
+              max_filesize = "1M",
+              -- Examples:
+              -- - ".git" (default)
+              -- - { ".git", "package.json", ".root" }
+              project_root_marker = ".git",
+              project_root_fallback = true,
+              search_casing = "--ignore-case",
+              additional_rg_options = {},
+              fallback_to_regex_highlighting = true,
+              ignore_paths = {},
+              additional_paths = {},
+              toggles = {
+                on_off = "<Leader>uG",
+              },
+              future_features = {
+                -- Workaround for
+                -- https://github.com/mikavilpas/blink-ripgrep.nvim/issues/185. This
+                -- is a temporary fix and will be removed in the future.
+                issue185_workaround = false,
+                backend = {
+                  use = "ripgrep",
+                },
+              },
+            },
+          },
+        },
+      },
+      keymap = {
+        -- 👇🏻👇🏻 (optional) add a keymap to invoke the search manually
+        ["<c-g>"] = {
+          function()
+            -- invoke manually, requires blink >v0.8.0
+            require("blink-cmp").show { providers = { "ripgrep" } }
+          end,
+        },
+      },
+    },
+  },
 }
