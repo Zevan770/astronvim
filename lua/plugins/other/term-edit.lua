c78be79592a1fcf4b1b87ae35686e003d5cc36fd
赵泽文(Zhao Zewen) <151506788+hw770@users.noreply.github.com>
2025-04-29 01:14:20 +0800

    add term-edit

---
+++ lua/plugins/other/term-edit.lua
@@ -0,0 +1,31 @@
+---@type LazySpec
+return {
+  {
+    "chomosuke/term-edit.nvim",
+    enabled = false,
+    event = "TermOpen",
+    opts = { prompt_end = "╰─ " },
+    version = "1.*",
+  },
+
+  {
+    "xb-bx/editable-term.nvim",
+    event = "TermOpen",
+    enabled = false,
+    opts = {
+      promts = {
+        ["^%(gdb%) "] = {}, -- gdb promt
+        ["^>>> "] = {}, -- python PS1
+        ["^... "] = {}, -- python PS2
+        ["╰─ "] = {},
+        -- ["some_other_prompt"] = {
+        --   keybinds = {
+        --     clear_current_line = "keys to clear the line",
+        --     goto_line_start = "keys to goto line start",
+        --     forward_char = "keys to move forward one character",
+        --   },
+        -- },
+      },
+    },
+  },
+}
