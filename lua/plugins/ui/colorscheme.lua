3dd678ea9aed493878ae3299d6c0baec59fbfa28
赵泽文(Zhao Zewen) <151506788+hw770@users.noreply.github.com>
2025-04-28 20:34:22 +0800

    feat(ui): colorscheme related

--- lua/plugins/ui/colorscheme.lua
+++ lua/plugins/ui/colorscheme.lua
@@ -39,6 +39,32 @@ return {
     lazy = true,
     ---@type CatppuccinOptions
     opts = {
+      transparent_background = true, -- disables setting the background color.
+      show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+      term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+      dim_inactive = {
+        enabled = true, -- dims the background color of inactive window
+        shade = "dark",
+        percentage = 0.35, -- percentage of the shade to apply to the inactive window
+      },
+      styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+        comments = { "italic" }, -- Change the style of comments
+        conditionals = { "italic" },
+        loops = {},
+        functions = {},
+        keywords = {},
+        strings = {},
+        variables = {},
+        numbers = {},
+        booleans = {},
+        properties = {},
+        types = {},
+        operators = {},
+        -- miscs = {}, -- Uncomment to turn off hard-coded styles
+      },
+      color_overrides = {},
+      custom_highlights = {},
+      default_integrations = true,
       integrations = {
         noice = true,
         mini = true,
--- lua/plugins/ui/fidget.lua
+++ lua/plugins/ui/fidget.lua
@@ -28,6 +28,9 @@ return {
 
       -- Options related to notification subsystem
       notification = {
+        window = {
+          winblend = 0,
+        },
         poll_rate = 10, -- How frequently to update and render notifications
         -- filter = vim.log.levels.INFO, -- Minimum notifications level
         history_size = 128, -- Number of removed messages to retain in history
