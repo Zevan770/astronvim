27b76d8fa6a2cd70d3e16d8c3a6d18700b97f9cb
赵泽文(Zhao Zewen) <151506788+hw770@users.noreply.github.com>
2025-05-08 21:32:42 +0800

    feat: snippets

--- lua/plugins/jump-edit/snippet.lua
+++ lua/plugins/jump-edit/snippet.lua
@@ -1,8 +1,18 @@
+local snippet_path = vim.fn.stdpath "config" .. "/snippets"
 return {
   {
     "chrisgrieser/nvim-scissors",
     opts = {
-      snippetDir = vim.fn.stdpath "config" .. "/lua/snippets",
+      snippetDir = snippet_path,
     },
+  },
+  {
+    "L3MON4D3/LuaSnip",
+    config = function(...)
+      require "astronvim.plugins.configs.luasnip"(...)
+      require("luasnip.loaders.from_vscode").lazy_load {
+        paths = { snippet_path },
+      }
+    end,
   },
 }
