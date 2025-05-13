e20659cf059502b19ad8d247a3562774c477a108
赵泽文(Zhao Zewen) <151506788+hw770@users.noreply.github.com>
2025-05-13 20:38:49 +0800

    tweak: smear-cursor

--- lua/plugins/ui/smear-cursor.lua
+++ lua/plugins/ui/smear-cursor.lua
@@ -3,10 +3,13 @@ return {
   event = "VeryLazy",
   cond = not my_utils.is_neovide and not my_utils.is_firenvim and not vim.env.KITTY_PID,
   -- enabled = false,
+  -- see ~/.local/share/nvim/lazy/smear-cursor.nvim/lua/smear_cursor/config.lua
   opts = {
     hide_target_hack = true,
     cursor_color = "none",
     legacy_computing_symbols_support = true,
+    min_horizontal_distance_smear = 8,
+    min_vertical_distance_smear = 3
   },
   specs = {
     -- disable mini.animate cursor
