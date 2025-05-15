3f56a253fe928eacfccbc870562818c8ba4622f2
赵泽文(Zhao Zewen) <151506788+hw770@users.noreply.github.com>
2025-05-16 00:43:32 +0800

    refactor blink

--- lua/plugins/lsp/blink.lua
+++ lua/plugins/lsp/blink.lua
@@ -25,7 +25,7 @@ return {
     end,
   },
   {
-    "saghen/blink.cmp",
+    "Saghen/blink.cmp",
     opts = {
       sources = {
         providers = {
@@ -41,89 +41,6 @@ return {
           },
         },
       },
-    },
-  },
-  {
-    "saghen/blink.cmp",
-    dependencies = {
-      {
-        "Kaiser-Yang/blink-cmp-git",
-      },
-    },
-    opts = {
-      sources = {
-        -- add 'git' to the list
-        default = { "git" },
-        providers = {
-          git = {
-            module = "blink-cmp-git",
-            name = "Git",
-            -- only enable this source when filetype is gitcommit, markdown, or 'octo'
-            enabled = function() return vim.tbl_contains({ "octo", "gitcommit", "markdown" }, vim.bo.filetype) end,
-            --- @module 'blink-cmp-git'
-            --- @type blink-cmp-git.Options
-            opts = {
-              -- options for the blink-cmp-git
-            },
-          },
-        },
-      },
-    },
-  },
-  {
-    "Saghen/blink.cmp",
-    dependencies = { "mikavilpas/blink-ripgrep.nvim" },
-    ---@type blink.cmp.Config
-    opts = {
-      sources = {
-        default = { "ripgrep" },
-        providers = {
-          -- 👇🏻👇🏻 add the ripgrep provider config below
-          ripgrep = {
-            module = "blink-ripgrep",
-            name = "Ripgrep",
-            score_offset = -1,
-            -- the options below are optional, some default values are shown
-            ---@module "blink-ripgrep"
-            ---@type blink-ripgrep.Options
-            opts = {
-              prefix_min_len = 3,
-              context_size = 5,
-              max_filesize = "1M",
-              -- Examples:
-              -- - ".git" (default)
-              -- - { ".git", "package.json", ".root" }
-              project_root_marker = ".git",
-              project_root_fallback = true,
-              search_casing = "--ignore-case",
-              additional_rg_options = {},
-              fallback_to_regex_highlighting = true,
-              ignore_paths = {},
-              additional_paths = {},
-              toggles = {
-                on_off = "<Leader>uG",
-              },
-              future_features = {
-                -- Workaround for
-                -- https://github.com/mikavilpas/blink-ripgrep.nvim/issues/185. This
-                -- is a temporary fix and will be removed in the future.
-                issue185_workaround = false,
-                backend = {
-                  use = "ripgrep",
-                },
-              },
-            },
-          },
-        },
-      },
-      -- keymap = {
-      --   ["<c-g>"] = {
-      --     function()
-      --       -- invoke manually, requires blink >v0.8.0
-      --       require("blink-cmp").show { providers = { "ripgrep" } }
-      --     end,
-      --   },
-      -- },
     },
   },
 }
