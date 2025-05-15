f24253afc8e5feb700a527fad490b85a3e5b84e5
赵泽文(Zhao Zewen) <151506788+hw770@users.noreply.github.com>
2025-05-16 04:06:19 +0800

    disable ai stuf

--- lua/os/server/disable.lua
+++ lua/os/server/disable.lua
@@ -2,6 +2,9 @@ local M = {}
 local disabled = {
   "obsidian.nvim",
   "codecompanion.nvim",
+  "copilot.lua",
+  "blink-copilot",
+  "mcphub.nvim",
   "avante.nvim",
   "im-select.nvim",
   "smear-cursor.nvim",
