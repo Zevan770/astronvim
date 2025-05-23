900ca2acb94f3e314991fb5a02bff79464306722
赵泽文(Zhao Zewen) <151506788+hw770@users.noreply.github.com>
2025-05-23 20:27:23 +0800

    feat: markview render in codecompanion

--- lua/plugins/languages/markdown.lua
+++ lua/plugins/languages/markdown.lua
@@ -73,6 +73,19 @@ return {
 
           icon_provider = "mini",
           filetypes = markview_on_ft,
+          ignore_buftypes = {},
+          condition = function(buffer)
+            return true
+            -- local ft, bt = vim.bo[buffer].ft, vim.bo[buffer].bt
+            --
+            -- if bt == "nofile" and ft == "codecompanion" then
+            --   return true
+            -- elseif bt == "nofile" then
+            --   return false
+            -- else
+            --   return true
+            -- end
+          end,
           callbacks = {
             on_enable = function(_, win)
               vim.wo[win].conceallevel = 0
