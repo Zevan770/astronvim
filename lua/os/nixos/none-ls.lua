5567003d1cee689017023c3527e76956559e2135
赵泽文(Zhao Zewen) <151506788+hw770@users.noreply.github.com>
2025-05-28 00:06:35 +0800

    lsp tweak

--- lua/os/nixos/none-ls.lua
+++ lua/os/nixos/none-ls.lua
@@ -9,6 +9,10 @@ return {
   opts = function(_, opts)
     -- opts variable is the default configuration table for the setup function call
     local null_ls = require "null-ls"
+    local code_actions = null_ls.builtins.code_actions
+    local diagnostics = null_ls.builtins.diagnostics
+    local hover = null_ls.builtins.hover
+    local completion = null_ls.builtins.completion
 
     -- Check supported formatters and linters
     -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/formatting
@@ -17,8 +21,19 @@ return {
     -- Only insert new sources, do not replace the existing ones
     -- (If you wish to replace, use `opts.sources = {}` instead of the `list_insert_unique` function)
     opts.sources = require("astrocore").list_insert_unique(opts.sources, {
-      null_ls.builtins.diagnostics.selene,
+      diagnostics.selene,
       require "none-ls.formatting.jq",
+
+      -- Common Code Actions
+      code_actions.gitsigns,
+      -- common refactoring actions based off the Refactoring book by Martin Fowler
+      code_actions.refactoring,
+      -- code_actions.proselint, -- English prose linter
+
+      -- nix
+      diagnostics.statix, -- Lints and suggestions for Nix.
+      code_actions.statix, -- Lints and suggestions for Nix.
+      diagnostics.deadnix, -- Scan Nix files for dead code.
     })
   end,
 }
