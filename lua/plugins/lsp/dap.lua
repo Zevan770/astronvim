3a1c1970249056843740ddd3055a13de6794eae9
赵泽文(Zhao Zewen) <151506788+hw770@users.noreply.github.com>
2025-05-30 02:05:54 +0800

    add snipe dap-view workspace-diagnostic

--- lua/plugins/lsp/dap.lua
+++ lua/plugins/lsp/dap.lua
@@ -15,4 +15,54 @@ return {
       },
     },
   },
+  {
+    "igorlfs/nvim-dap-view",
+    lazy = true,
+    opts = {},
+    specs = {
+      {
+        "rcarriga/nvim-dap-ui",
+        enabled = false,
+      },
+    },
+    config = function(_, opts)
+      local dap, dv = require "dap", require "dap-view"
+      dv.setup(opts)
+
+      dap.listeners.before.attach["dap-view-config"] = dv.open
+      dap.listeners.before.launch["dap-view-config"] = dv.open
+      dap.listeners.before.event_terminated["dap-view-config"] = dv.close
+      dap.listeners.before.event_exited["dap-view-config"] = dv.close
+    end,
+  },
+  {
+    "theHamsta/nvim-dap-virtual-text",
+    dependencies = "mfussenegger/nvim-dap",
+    opts = {
+      highlight_changed_variables = true,
+      highlight_new_as_changed = false,
+      only_first_definition = false,
+      all_references = false,
+    },
+    keys = {
+      {
+        "<leader>dv",
+        function() require("nvim-dap-virtual-text").toggle() end,
+        desc = "󱂬 Toggle virtual text",
+      },
+    },
+    config = function(_, opts)
+      local dapVirtText = require "nvim-dap-virtual-text"
+      dapVirtText.setup(opts)
+
+      -- auto-disable/enable
+      require("dap").listeners.after.disconnect.dapVirtText = dapVirtText.disable
+    end,
+    init = function()
+      vim.api.nvim_create_autocmd({ "ColorScheme", "VimEnter" }, {
+        desc = "User: Change `NvimDapVirtualText` color",
+        callback = function() vim.api.nvim_set_hl(0, "NvimDapVirtualText", { link = "DiagnosticSignInfo" }) end,
+      })
+    end,
+  },
 }
--- lua/plugins/lsp/trouble.lua
+++ lua/plugins/lsp/trouble.lua
@@ -2,6 +2,21 @@
 return {
   { import = "astrocommunity.diagnostics.trouble-nvim" },
   {
+    "artemave/workspace-diagnostics.nvim",
+    opts = {},
+    keys = {
+      {
+        "<leader>xd",
+        function()
+          for _, client in ipairs(vim.lsp.get_clients { bufnr = 0 }) do
+            require("workspace-diagnostics").populate_workspace_diagnostics(client, 0)
+          end
+        end,
+        desc = "Populate workspace diagnostics",
+      },
+    },
+  },
+  {
     "folke/trouble.nvim",
     opts = {
       auto_open = false, -- auto open when there are items
