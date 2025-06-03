68e6bfd3d59b057e3da61c3bfed18e53daa4520d
赵泽文(Zhao Zewen) <151506788+hw770@users.noreply.github.com>
2025-06-03 21:22:29 +0800

    try jieba.vim

--- lua/plugins/jump-edit/spider.lua
+++ lua/plugins/jump-edit/spider.lua
@@ -7,4 +7,15 @@ return {
       { "b", "<cmd>lua require('spider').motion('b')<CR>", mode = { "n", "o", "x" } },
     },
   },
+
+  {
+    "kkew3/jieba.vim",
+    tag = "v1.0.5",
+    enabled = false,
+    build = "bash ./build.sh",
+    init = function()
+      vim.g.jieba_vim_lazy = 1
+      vim.g.jieba_vim_keymap = 1
+    end,
+  },
 }
