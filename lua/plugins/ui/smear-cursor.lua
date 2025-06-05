1299d77c9aeac1536c7945b3d07e4b26278ccb5b
赵泽文(Zhao Zewen) <151506788+hw770@users.noreply.github.com>
2025-06-05 19:49:15 +0800

    smear-cursor faster

--- lua/plugins/ui/smear-cursor.lua
+++ lua/plugins/ui/smear-cursor.lua
@@ -8,6 +8,11 @@ return {
     opts = {
       hide_target_hack = true,
       cursor_color = "none",
+      stiffness = 0.8, -- 0.6      [0, 1]
+      trailing_stiffness = 0.5, -- 0.4      [0, 1]
+      stiffness_insert_mode = 0.6, -- 0.4      [0, 1]
+      trailing_stiffness_insert_mode = 0.6, -- 0.4      [0, 1]
+      distance_stop_animating = 0.5, -- 0.1      > 0
       -- legacy_computing_symbols_support = true,
       -- smear_horizontally = true,
       -- smear_vertically = true,
