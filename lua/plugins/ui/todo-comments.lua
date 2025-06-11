0e245a5240a61f1c96be7c59f94c7b94064b6d7c
赵泽文(Zhao Zewen) <151506788+hw770@users.noreply.github.com>
2025-06-11 21:21:06 +0800

    todo tweak

---
+++ lua/plugins/ui/todo-comments.lua
@@ -0,0 +1,14 @@
+return {
+  "folke/todo-comments.nvim",
+  ---@diagnostic disable: missing-fields
+  ---@module "todo-comments"
+  ---@type TodoOptions
+  opts = {
+    keywords = {
+      region = {
+        color = "default",
+        alt = { [[region]], [[endregion]] },
+      },
+    },
+  },
+}
