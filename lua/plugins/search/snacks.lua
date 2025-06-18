4af257c2557d72a84f576c1b41c41cf968aae230
赵泽文(Zhao Zewen) <151506788+hw770@users.noreply.github.com>
2025-06-19 07:50:41 +0800

    style snacks

--- lua/plugins/search/snacks.lua
+++ lua/plugins/search/snacks.lua
@@ -130,7 +130,7 @@ return {
             desc = maps.n["<Leader>fb"].desc,
           }
           maps.n["<A-b>"] = maps.n["<Leader>fb"]
-          maps.n["<A-x>"] = maps.n["<Leader>fC"]
+          -- maps.n["<A-x>"] = maps.n["<Leader>fC"]
 
           maps.n["<Leader>fc"] = { function() Snacks.picker.commands() end, desc = "Find Commands" }
           maps.n["<Leader>fr"] = { function() Snacks.picker.recent() end, desc = "Find Recents" }
@@ -257,19 +257,19 @@ return {
     opts = function(_, opts)
       local get_icon = require("astroui").get_icon
 
-      local buaa_logo_section
-      local buaa_logo_path = vim.fn.stdpath "config" .. "/resorces/Beihang-university-logo.svg"
-      if my_utils.is_windows or not vim.fn.executable "chafa" then
-        buaa_logo_section = {
-          section = "header",
-        }
-      else
-        buaa_logo_section = {
-          section = "terminal",
-          cmd = "chafa --format symbols --symbols vhalf " .. buaa_logo_path .. " --size 60x30; sleep .1",
-          height = 30,
-        }
-      end
+      -- local buaa_logo_section
+      -- local buaa_logo_path = vim.fn.stdpath "config" .. "/resorces/Beihang-university-logo.svg"
+      -- if my_utils.is_windows or not vim.fn.executable "chafa" then
+      --   buaa_logo_section = {
+      --     section = "header",
+      --   }
+      -- else
+      --   buaa_logo_section = {
+      --     section = "terminal",
+      --     cmd = "chafa --format symbols --symbols vhalf " .. buaa_logo_path .. " --size 60x30; sleep .1",
+      --     height = 30,
+      --   }
+      -- end
 
       opts.dashboard.preset.keys = {
         { key = "n", action = "<Leader>n", icon = get_icon("FileNew", 0, true), desc = "New File  " },
@@ -387,45 +387,6 @@ return {
   --   },
   -- },
   --
-
-  {
-    "dawsers/file-history.nvim",
-    event = "User AstroFile",
-    enabled = not my_utils.is_windows,
-    config = function()
-      local file_history = require "file_history"
-      file_history.setup {
-        -- These are the default values, change them if needed
-        -- Location where the plugin will create your file history repository
-        backup_dir = "~/.file-history-git",
-        -- command line to execute git
-        git_cmd = "git",
-        -- If you want to override the automatic query for hostname, change this
-        -- option. By default (nil), the plugin gets the host name for the computer
-        -- it is running on.
-        --
-        -- You should only modify this value if you understand the following:
-        -- This plugin writes a backup copy of every file you edit in neovim, not
-        -- just your coding projects. When copying the file-history repository from
-        -- one computer to another, having the hostname added to each file in the
-        -- repo prevents you from messing the history of files that should be unique
-        -- to that computer (host). For example, configuration and system files
-        -- will probably be different in part or fully. So, even though it may
-        -- make sense for coding projects to be able to move the database and
-        -- disregard the host name, in many cases you will be editing other types
-        -- of files, where keeping the correct host name will help you recover
-        -- from mistakes.
-        hostname = nil,
-      }
-    -- There are no default key maps, this is an example
-    -- stylua: ignore start
-    vim.keymap.set('n', '<leader>Bb',function() file_history.backup() end, { silent = true, desc = 'named backup for file' })
-    vim.keymap.set('n', '<leader>Bh', function() file_history.history() end, { silent = true, desc = 'local history of file' })
-    vim.keymap.set('n', '<leader>Bf', function() file_history.files() end, { silent = true, desc = 'local history files in repo' })
-    vim.keymap.set('n', '<leader>Bq', function() file_history.query() end, { silent = true, desc = 'local history query' })
-      -- stylua: ignore end
-    end,
-  },
 
   -- {
   --   "2kabhishek/nerdy.nvim",
