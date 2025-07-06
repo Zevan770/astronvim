a04eff6d60e9dd61a3f32076e4660f027375f277
赵泽文(Zhao Zewen) <151506788+hw770@users.noreply.github.com>
2025-07-06 21:42:14 +0800

    disable focus.nvim

--- lua/plugins/ui/focus.lua
+++ lua/plugins/ui/focus.lua
@@ -5,19 +5,19 @@ return {
     "nvim-focus/focus.nvim",
     version = false,
     event = "WinLeave",
-    -- enabled = false,
+    enabled = false,
     init = function()
       local ignore_buftypes = { "nofile", "prompt", "popup" }
 
       local augroup = vim.api.nvim_create_augroup("FocusDisable", { clear = true })
 
-      vim.api.nvim_create_autocmd({ "WinEnter", "BufWinEnter" }, {
+      vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
         group = augroup,
         callback = function(_)
           if vim.tbl_contains(ignore_buftypes, vim.bo.buftype) then
-            vim.w.focus_disable = true
-            -- else
-            --   vim.w.focus_disable = false
+            vim.b.focus_disable = true
+          else
+            vim.b.focus_disable = false
           end
         end,
         desc = "Disable focus autoresize for BufType",
@@ -59,14 +59,14 @@ return {
         hybridnumber = false, -- Display hybrid line numbers in the focussed window only
         absolutenumber_unfocussed = false, -- Preserve absolute numbers in the unfocussed windows
 
-        cursorline = true, -- Display a cursorline in the focussed window only
+        cursorline = false, -- Display a cursorline in the focussed window only
         cursorcolumn = false, -- Display cursorcolumn in the focussed window only
         colorcolumn = {
           enable = false, -- Display colorcolumn in the foccused window only
           list = "+1", -- Set the comma-saperated list for the colorcolumn
         },
         signcolumn = false, -- Display signcolumn in the focussed window only
-        winhighlight = false, -- Auto highlighting for focussed/unfocussed windows
+        winhighlight = true, -- Auto highlighting for focussed/unfocussed windows
       },
     },
   },
