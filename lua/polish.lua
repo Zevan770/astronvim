051e2f98435ca252f61df0861bc220459757f09b
赵泽文(Zhao Zewen) <151506788+hw770@users.noreply.github.com>
2025-07-07 23:49:32 +0800

    clear clipboard-tools: use string directly

--- lua/polish.lua
+++ lua/polish.lua
@@ -2,19 +2,6 @@
 -- things like custom filetypes. This just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
 
-local clip_win32 = {
-  name = "win32yank-wsl",
-  copy = {
-    ["+"] = "win32yank.exe -i --crlf",
-    ["*"] = "win32yank.exe -i --crlf",
-  },
-  paste = {
-    ["+"] = "win32yank.exe -o --lf",
-    ["*"] = "win32yank.exe -o --lf",
-  },
-  cache_enabled = true,
-}
-
 local clip_osc52 = {
   name = "OSC 52",
   copy = {
@@ -27,31 +14,6 @@ local clip_osc52 = {
   },
 }
 
-local clip_termux = {
-  name = "termux",
-  copy = {
-    ["+"] = "termux-clipboard-set",
-    ["*"] = "termux-clipboard-set",
-  },
-  paste = {
-    ["+"] = "termux-clipboard-get",
-    ["*"] = "termux-clipboard-get",
-  },
-}
-
-local clip_windows = {
-  name = "WslClipboard",
-  copy = {
-    ["+"] = "clip.exe",
-    ["*"] = "clip.exe",
-  },
-  paste = {
-    ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).ToString().Replace("`r", ""))',
-    ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).ToString().Replace("`r", ""))',
-  },
-  cache_enabled = true,
-}
-
 vim.opt.clipboard = vim.env.SSH_TTY and "" or "unnamedplus" -- Sync with system clipboard
-vim.g.clipboard = vim.fn.has "android" == 1 and clip_termux or clip_win32
+vim.g.clipboard = vim.fn.has "android" == 1 and "termux" or "win32yank"
 if my_utils.is_vscode then vim.g.clipboard = vim.g.vscode_clipboard end
