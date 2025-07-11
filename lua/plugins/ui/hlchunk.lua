11505d168b97c758a6801d73a9295cb61be29b17
赵泽文(Zhao Zewen) <151506788+hw770@users.noreply.github.com>
2025-07-12 03:16:05 +0800

    add hlchunk, replace snacks.indentscope

---
+++ lua/plugins/ui/hlchunk.lua
@@ -0,0 +1,49 @@
+---@type LazySpec
+return {
+  {
+    "shellRaining/hlchunk.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    opts = function()
+      local palette = require "catppuccin.palettes.mocha"
+      local excluded_ft = {
+        ["neo-tree"] = true,
+        snacks_dashboard = true,
+        fidget = true,
+        help = true,
+        snacks_picker_preview = true,
+      }
+
+      local indent_colors = {
+        palette.surface0,
+        palette.surface1,
+        palette.surface2,
+        palette.overlay0,
+        palette.overlay1,
+        palette.overlay2,
+        palette.subtext0,
+        palette.subtext1,
+        palette.text,
+      }
+      return {
+        chunk = {
+          delay = 100,
+          enable = true,
+          exclude_filetypes = excluded_ft,
+          style = palette.lavender,
+        },
+        indent = {
+          enable = true,
+          exclude_filetypes = excluded_ft,
+          style = indent_colors,
+        },
+      }
+    end,
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+  },
+  {
+    "folke/snacks.nvim",
+    opts = {
+      indent = { enabled = false },
+    },
+  },
+}
