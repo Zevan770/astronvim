71e44a959ef34743339672033fc28bad352abbe8
赵泽文(Zhao Zewen) <151506788+hw770@users.noreply.github.com>
2025-08-20 06:58:30 +0800

    lsp: powershell_es tweaks

--- lua/plugins/languages/ps1.lua
+++ lua/plugins/languages/ps1.lua
@@ -1,23 +1,52 @@
+---@diagnostic disable: missing-fields
 if vim.fn.has "unix" == 1 then return {} end
 return {
-  -- {
-  --   "AstroNvim/astrocommunity",
-  --   { import = "astrocommunity.pack.ps1" },
-  -- },
+  { import = "astrocommunity.pack.ps1" },
   {
-    "TheLeoP/powershell.nvim",
-    -- ft = "ps1",
-    ---@type powershell.user_config
+    "AstroNvim/astrolsp",
+    ---@type AstroLSPOpts
     opts = {
-      capabilities = require("blink.cmp").get_lsp_capabilities(nil, true),
-      bundle_path = vim.fn.stdpath "data" .. "/mason/packages/powershell-editor-services",
-      init_options = {
-        enableProfileLoading = false,
+      servers = {
+        "powershell_es",
       },
-      settings = {
-        enableProfileLoading = false,
+      config = {
+        powershell_es = (function()
+          local bundle_path = vim.fn.stdpath "data" .. "/mason/packages/powershell-editor-services"
+          local shell = "pwsh"
+
+          local temp_path = vim.fn.stdpath "cache"
+          local function make_cmd()
+            if bundle_path ~= nil then
+              -- https://github.com/neovim/nvim-lspconfig/pull/3920
+              local command_fmt =
+                [[& '%s/PowerShellEditorServices/Start-EditorServices.ps1' -BundledModulesPath '%s' -LogPath '%s/powershell_es.log' -SessionDetailsPath '%s/powershell_es.session.json' -FeatureFlags @() -AdditionalModules @() -HostName nvim -HostProfileId 0 -HostVersion 1.0.0 -Stdio -LogLevel Normal]]
+              local command = command_fmt:format(bundle_path, bundle_path, temp_path, temp_path)
+              return { shell, "-NoLogo", "-NoProfile", "-Command", command }
+            end
+          end
+
+          return {
+            bundle_path = bundle_path,
+            cmd = make_cmd(),
+            shell = shell,
+          }
+        end)(),
       },
     },
   },
-  { "PProvost/vim-ps1", ft = "ps1" },
+  -- {
+  --   "TheLeoP/powershell.nvim",
+  --   -- ft = "ps1",
+  --   ---@type powershell.user_config
+  --   opts = {
+  --     capabilities = require("blink.cmp").get_lsp_capabilities(nil, true),
+  --     bundle_path = vim.fn.stdpath "data" .. "/mason/packages/powershell-editor-services",
+  --     init_options = {
+  --       enableProfileLoading = false,
+  --     },
+  --     settings = {
+  --       enableProfileLoading = false,
+  --     },
+  --   },
+  -- },
 }
