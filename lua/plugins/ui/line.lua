eb1d98cd4f0cebfbcaec545039904cb0445f7585
赵泽文(Zhao Zewen) <151506788+hw770@users.noreply.github.com>
2025-08-25 01:55:56 +0800

    tweak: colorful-winsep

--- lua/plugins/ui/line.lua
+++ lua/plugins/ui/line.lua
@@ -3,8 +3,34 @@ return {
 
   {
     "nvim-zh/colorful-winsep.nvim",
-    enabled = false,
-    config = true,
+    -- enabled = false,
+    config = function(_, opts) require("colorful-winsep").setup() end,
+    opts = {
+      -- highlight for Window separator
+      hi = {
+        link = "FloatBorder",
+      },
+      -- This plugin will not be activated for filetype in the following table.
+      no_exec_files = {
+        "packer",
+        "TelescopePrompt",
+        "mason",
+        "NvimTree",
+      },
+      -- Symbols for separator lines, the order: horizontal, vertical, top left, top right, bottom left, bottom right.
+      -- symbols = vim.g.winborder == "single" and { "─", "│", "┌", "┐", "└", "┘" }
+      --   or { "─", "│", "╭", "╮", "╰", "╯" },
+      -- Smooth moving switch
+      smooth = false,
+      border = "bold",
+      zindex = 100,
+      anchor = {
+        left = { height = 1, x = -1, y = -1 },
+        right = { height = 1, x = -1, y = 0 },
+        up = { width = 0, x = -1, y = 0 },
+        bottom = { width = 0, x = 1, y = 0 },
+      },
+    },
     event = { "WinLeave" },
   },
 
