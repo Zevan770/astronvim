f63033a8485ccb31cf51ac884440513e2af29732
赵泽文(Zhao Zewen) <151506788+hw770@users.noreply.github.com>
2025-08-27 18:07:17 +0800

    tweak markit.nvim(still disabled)

--- lua/plugins/jump-edit/markit.lua
+++ lua/plugins/jump-edit/markit.lua
@@ -2,10 +2,6 @@ if true then return {} end
 ---@type LazySpec
 return {
   {
-    "chentoast/marks.nvim",
-    enabled = false,
-  },
-  {
     "2kabhishek/markit.nvim",
     config = function()
       require("markit").setup {
@@ -26,23 +22,32 @@ return {
         -- can be either a table with all/none of the keys, or a single number, in which case
         -- the priority applies to all marks.
         -- default 10.
-        sign_priority = { lower = 10, upper = 15, builtin = 8, bookmark = 20 },
+        sign_priority = { lower = 2, upper = 3, builtin = 1, bookmark = 4 },
         -- disables mark tracking for specific filetypes. default {}
         excluded_filetypes = {},
         -- disables mark tracking for specific buftypes. default {}
-        excluded_buftypes = {},
+        excluded_buftypes = {
+          "nofile",
+          "terminal",
+        },
         -- marks.nvim allows you to configure up to 10 bookmark groups, each with its own
         -- sign/virttext. Bookmarks can be used to group together positions and quickly move
         -- across multiple buffers. default sign is '!@#$%^&*()' (from 0 to 9), and
         -- default virt_text is "".
-        bookmark_0 = {
+        bookmark_1 = {
           sign = "⚑",
-          virt_text = "hello world",
+          virt_text = "Bug",
           -- explicitly prompt for a virtual line annotation when setting a bookmark from this group.
           -- defaults to false.
-          annotate = false,
+          annotate = true,
         },
-        mappings = {},
+        mappings = {
+          -- preview = "',",
+          -- delete_line = "dmm",
+          -- annotate = "cm",
+          set = "M",
+          toggle = "m",
+        },
       }
     end,
 
