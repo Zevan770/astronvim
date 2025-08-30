25520f7bf1ed5b5f746f665f4aa704eb08fcc9ed
赵泽文(Zhao Zewen) <151506788+hw770@users.noreply.github.com>
2025-08-31 02:01:34 +0800

    add trim.nvim

---
+++ lua/plugins/jump-edit/trim.lua
@@ -0,0 +1,17 @@
+return {
+
+  -- trim.nvim [auto trim spaces]
+  -- https://github.com/cappyzawa/trim.nvim
+  {
+    "cappyzawa/trim.nvim",
+    event = "BufWrite",
+    opts = {
+      trim_on_write = true,
+      trim_trailing = true,
+      trim_last_line = false,
+      trim_first_line = false,
+      -- ft_blocklist = { "markdown", "text", "org", "tex", "asciidoc", "rst" },
+      -- patterns = {[[%s/\(\n\n\)\n\+/\1/]]}, -- Only one consecutive bl
+    },
+  },
+}
