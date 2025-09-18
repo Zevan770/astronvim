f553e03844c1fc451ebb49b14082c6ce357def78
赵泽文(Zhao Zewen) <151506788+hw770@users.noreply.github.com>
2025-09-18 21:31:45 +0800

    feat: mini.ai and mini.surround buffer local config

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,12 +1,20 @@
-vim.bo.tabstop = 4
-vim.bo.shiftwidth = 4
-vim.opt_local.wrap = true
+-- vim.bo.tabstop = 4
+-- vim.bo.shiftwidth = 4
+vim.wo.wrap = true
 
 local spec_pair = require("mini.ai").gen_spec.pair
 vim.b.miniai_config = {
   custom_textobjects = {
-    ["*"] = spec_pair("*", "*", { type = "greedy" }),
+    ["8"] = spec_pair("*", "*", { type = "greedy" }),
     ["_"] = spec_pair("_", "_", { type = "greedy" }),
     ["="] = spec_pair("=", "=", { type = "greedy" }),
+  },
+}
+vim.b.minisurround_config = {
+  custom_surroundings = {
+    ["8"] = {
+      input = { "%*%*().-()%*%*" },
+      output = { left = "**", right = "**" },
+    },
   },
 }
