e92d2c6d0433173461122f01051e9b38521cfe9d
赵泽文(Zhao Zewen) <151506788+hw770@users.noreply.github.com>
2025-09-19 05:14:50 +0800

    windows vim.opt.shell pwsh

--- lua/os/windows/windows.lua
+++ lua/os/windows/windows.lua
@@ -5,7 +5,12 @@ return {
       return require("astrocore").extend_tbl(opts, {
         options = {
           opt = {
-            -- shell = vim.fn.executable "pwsh.exe" == 1 and "pwsh.exe" or "powershell.exe",
+            shell = vim.fn.executable "pwsh.exe" == 1 and "pwsh.exe" or "powershell.exe",
+            shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+            shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+            shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
+            shellquote = "",
+            shellxquote = "",
             -- shellcmdflag = "-NoLogo -NonInteractive -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues['Out-File:Encoding']='utf8';$PSStyle.OutputRendering='plaintext';Remove-Alias -Force -ErrorAction SilentlyContinue tee;",
             -- -- shellcmdflag = "-NoLogo -NonInteractive -ExecutionPolicy RemoteSigned -Command $PSStyle.OutputRendering='plaintext';Remove-Alias -Force -ErrorAction SilentlyContinue tee;",
             -- shellredir = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode',
