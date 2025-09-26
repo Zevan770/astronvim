e9da61b7b9254d0e06167e11fc98f5a0f9d7aecd
赵泽文(Zhao Zewen) <151506788+hw770@users.noreply.github.com>
2025-09-26 22:53:55 +0800

    update vectorcode

--- lua/plugins/other/codecompanion/vectorcode.lua
+++ lua/plugins/other/codecompanion/vectorcode.lua
@@ -2,7 +2,7 @@
 return {
   {
     "Davidyz/VectorCode",
-    version = "0.7.7", -- optional, depending on whether you're on nightly or release
+    version = "0.7.15", -- optional, depending on whether you're on nightly or release
     dependencies = { "nvim-lua/plenary.nvim" },
     cmd = "VectorCode", -- if you're lazy-loading VectorCode
     lazy = true,
