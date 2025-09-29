be071159331ea81b52b64002424783e7c4e70aa4
赵泽文(Zhao Zewen) <151506788+hw770@users.noreply.github.com>
2025-09-30 01:48:15 +0800

    fix: neoconf hack
    update

--- lua/plugins/lsp/neoconf.lua
+++ lua/plugins/lsp/neoconf.lua
@@ -1,6 +1,16 @@
 ---@type LazySpec
 return {
   {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      { "folke/neoconf.nvim", lazy = true, opts = {} },
+    },
+    cmd = function(_, cmds) -- HACK: lazy load lspconfig on `:Neoconf` if neoconf is available
+      if require("lazy.core.config").spec.plugins["neoconf.nvim"] then table.insert(cmds, "Neoconf") end
+      vim.list_extend(cmds, { "LspInfo", "LspLog", "LspStart" }) -- add normal `nvim-lspconfig` commands
+    end,
+  },
+  {
     "folke/neoconf.nvim",
     ---@module "neoconf"
     ---@type Config
