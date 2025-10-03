60f72969f9453afaee006edfb71ac21f9f83cb8a
赵泽文(Zhao Zewen) <151506788+hw770@users.noreply.github.com>
2025-10-04 03:32:31 +0800

    add codesettings.nvim to replace neoconf.nvim

---
+++ lua/plugins/lsp/codesettings.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "mrjones2014/codesettings.nvim",
+    -- these are the default settings just set `opts = {}` to use defaults
+    opts = {
+      ---Look for these config files
+      config_file_paths = { ".vscode/settings.json", "codesettings.json", "lspsettings.json" },
+      ---Integrate with jsonls to provide LSP completion for LSP settings based on schemas
+      jsonls_integration = true,
+      ---Set filetype to jsonc when opening a file specified by `config_file_paths`,
+      ---make sure you have the jsonc tree-sitter parser installed for highlighting
+      jsonc_filetype = true,
+    },
+    event = "VeryLazy",
+  },
+}
