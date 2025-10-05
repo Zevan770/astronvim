549238c536097728712e6a396cdbcbe2a4c4b8fb
Zevan770 <151506788+hw770@users.noreply.github.com>
2025-10-06 03:19:23 +0800

    tweak mini.files keymap

--- lua/plugins/other/explorer.lua
+++ lua/plugins/other/explorer.lua
@@ -166,6 +166,9 @@ return {
     opts = {
       mappings = {
         synchronize = "<c-s>",
+        go_in = "<right>",
+        go_out = "<left>",
+        mark_set = "M",
       },
       windows = {
         preview = true,
@@ -198,6 +201,31 @@ return {
                   end
                 end,
                 desc = "Explorer",
+              },
+            },
+          },
+          autocmds = {
+            zev_mini_files_keymaps = {
+              {
+                event = "User",
+                pattern = "MiniFilesBufferCreate",
+                callback = function(args)
+                  local buf_id = args.data.buf_id
+
+                  local show_dotfiles = true
+
+                  local filter_show = function(fs_entry) return true end
+
+                  local filter_hide = function(fs_entry) return not vim.startswith(fs_entry.name, ".") end
+
+                  local toggle_dotfiles = function()
+                    show_dotfiles = not show_dotfiles
+                    local new_filter = show_dotfiles and filter_show or filter_hide
+                    MiniFiles.refresh { content = { filter = new_filter } }
+                  end
+
+                  vim.keymap.set("n", "g.", toggle_dotfiles, { buffer = buf_id })
+                end,
               },
             },
           },
