5db3cccf4ad137730dd885d3702f6ce408be7fca
Zevan770 <151506788+hw770@users.noreply.github.com>
2025-10-09 03:22:57 +0800

    refactor: ps1

--- lua/plugins/languages/ps1.lua
+++ lua/plugins/languages/ps1.lua
@@ -7,23 +7,23 @@ return {
     ---@type AstroLSPOpts
     opts = {
       servers = {
-        "powershell_es",
+        -- "powershell_es",
       },
     },
   },
-  -- {
-  --   "TheLeoP/powershell.nvim",
-  --   -- ft = "ps1",
-  --   ---@type powershell.user_config
-  --   opts = {
-  --     capabilities = require("blink.cmp").get_lsp_capabilities(nil, true),
-  --     bundle_path = vim.fn.stdpath "data" .. "/mason/packages/powershell-editor-services",
-  --     init_options = {
-  --       enableProfileLoading = false,
-  --     },
-  --     settings = {
-  --       enableProfileLoading = false,
-  --     },
-  --   },
-  -- },
+  {
+    "TheLeoP/powershell.nvim",
+    -- ft = "ps1",
+    ---@type powershell.user_config
+    opts = {
+      capabilities = require("blink.cmp").get_lsp_capabilities(nil, true),
+      bundle_path = vim.fn.stdpath "data" .. "/mason/packages/powershell-editor-services",
+      init_options = {
+        enableProfileLoading = false,
+      },
+      settings = {
+        enableProfileLoading = false,
+      },
+    },
+  },
 }
