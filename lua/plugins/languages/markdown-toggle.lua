a752b406e5e7130e8b4c6fe44a5b6cbf47aff1bf
Zevan770 <151506788+hw770@users.noreply.github.com>
2025-10-10 01:53:18 +0800

    fix: make markdown-toggle work

--- lua/plugins/languages/markdown-toggle.lua
+++ lua/plugins/languages/markdown-toggle.lua
@@ -8,7 +8,7 @@ return {
         -- If true, the auto-setup for the default keymaps is enabled
         use_default_keymaps = false,
         -- The keymaps are valid only for these filetypes
-        filetypes = { "markdown", "markdown.mdx" },
+        filetypes = markdown_ft,
 
         -- The list marks table used in cycle-mode (list_table[1] is used as the default list-mark)
         list_table = { "-", "+", "*", "=" },
@@ -41,38 +41,39 @@ return {
     end,
     keys = function()
       local toggle = require "markdown-toggle"
+      ---@type LazyKeysSpec
       return {
         -- stylua: ignore start
         -- Settings mappings
-        { mode = "n", "<localleader>uu", function() toggle.switch_unmarked_only() end,     desc = "Toggle unmarked-only mode",    ft = markdown_ft },
-        { mode = "n", "<localleader>ub", function() toggle.switch_blankhead_skip() end,    desc = "Toggle blankhead-skip mode",   ft = markdown_ft },
-        { mode = "n", "<localleader>us", function() toggle.switch_auto_samestate() end,    desc = "Toggle auto-samestate mode",   ft = markdown_ft },
-        { mode = "n", "<localleader>ul", function() toggle.switch_cycle_list_table() end,  desc = "Toggle cycle-list-table mode", ft = markdown_ft },
-        { mode = "n", "<localleader>ux", function() toggle.switch_cycle_box_table() end,   desc = "Toggle cycle-box-table mode",  ft = markdown_ft },
-        { mode = "n", "<localleader>uc", function() toggle.switch_list_before_box() end,   desc = "Toggle list-before-box mode",  ft = markdown_ft },
+        { "<localleader>uu", function() toggle.switch_unmarked_only() end,     mode = "n", desc = "Toggle unmarked-only mode",    ft = markdown_ft },
+        { "<localleader>ub", function() toggle.switch_blankhead_skip() end,    mode = "n", desc = "Toggle blankhead-skip mode",   ft = markdown_ft },
+        { "<localleader>us", function() toggle.switch_auto_samestate() end,    mode = "n", desc = "Toggle auto-samestate mode",   ft = markdown_ft },
+        { "<localleader>ul", function() toggle.switch_cycle_list_table() end,  mode = "n", desc = "Toggle cycle-list-table mode", ft = markdown_ft },
+        { "<localleader>ux", function() toggle.switch_cycle_box_table() end,   mode = "n", desc = "Toggle cycle-box-table mode",  ft = markdown_ft },
+        { "<localleader>uc", function() toggle.switch_list_before_box() end,   mode = "n", desc = "Toggle list-before-box mode",  ft = markdown_ft },
 
         -- Normal mode with dot-repeat
-        { mode = "n", "<localleader>q",  function() toggle.quote_dot() end,                desc = "Toggle quote",                 ft = markdown_ft, expr = true },
-        { mode = "n", "<localleader>l",  function() toggle.list_dot() end,                 desc = "Toggle list",                  ft = markdown_ft, expr = true },
-        { mode = "n", "<localleader>s",  function() toggle.list_cycle_dot() end,           desc = "Toggle list cycle",            ft = markdown_ft, expr = true },
-        { mode = "n", "<localleader>n",  function() toggle.olist_dot() end,                desc = "Toggle ordered list",          ft = markdown_ft, expr = true },
-        { mode = "n", "<localleader>x",  function() toggle.checkbox_dot() end,             desc = "Toggle checkbox",              ft = markdown_ft, expr = true },
-        { mode = "n", "<localleader>c",  function() toggle.checkbox_cycle_dot() end,       desc = "Toggle checkbox cycle",        ft = markdown_ft, expr = true },
-        { mode = "n", "<localleader>h",  function() toggle.heading_dot() end,              desc = "Toggle heading",               ft = markdown_ft, expr = true },
+        { "<localleader>q",  toggle.quote_dot,                                 mode = "n", desc = "Toggle quote",                 ft = markdown_ft,  expr = true  },
+        { "<localleader>l",  toggle.list_dot,                                  mode = "n", desc = "Toggle list",                  ft = markdown_ft,  expr = true  },
+        { "<localleader>s",  toggle.list_cycle_dot,                            mode = "n", desc = "Toggle list cycle",            ft = markdown_ft,  expr = true  },
+        { "<localleader>n",  toggle.olist_dot,                                 mode = "n", desc = "Toggle ordered list",          ft = markdown_ft,  expr = true  },
+        { "<localleader>x",  toggle.checkbox_dot,                              mode = "n", desc = "Toggle checkbox",              ft = markdown_ft,  expr = true  },
+        { "<localleader>c",  toggle.checkbox_cycle_dot,                        mode = "n", desc = "Toggle checkbox cycle",        ft = markdown_ft,  expr = true  },
+        { "<localleader>h",  toggle.heading_dot,                               mode = "n", desc = "Toggle heading",               ft = markdown_ft,  expr = true  },
 
         -- Visual mode without dot-repeat
-        { mode = "x", "<localleader>q",  function() toggle.quote() end,                    desc = "Toggle quote",                 ft = markdown_ft },
-        { mode = "x", "<localleader>l",  function() toggle.list() end,                     desc = "Toggle list",                  ft = markdown_ft },
-        { mode = "x", "<localleader>s",  function() toggle.list_cycle() end,               desc = "Toggle list cycle",            ft = markdown_ft },
-        { mode = "x", "<localleader>n",  function() toggle.olist() end,                    desc = "Toggle ordered list",          ft = markdown_ft },
-        { mode = "x", "<localleader>x",  function() toggle.checkbox() end,                 desc = "Toggle checkbox",              ft = markdown_ft },
-        { mode = "x", "<localleader>c",  function() toggle.checkbox_cycle() end,           desc = "Toggle checkbox cycle",        ft = markdown_ft },
-        { mode = "x", "<localleader>h",  function() toggle.heading() end,                  desc = "Toggle heading",               ft = markdown_ft },
+        { "<localleader>q",  function() toggle.quote() end,                    mode = "x", desc = "Toggle quote",                 ft = markdown_ft },
+        { "<localleader>l",  function() toggle.list() end,                     mode = "x", desc = "Toggle list",                  ft = markdown_ft },
+        { "<localleader>s",  function() toggle.list_cycle() end,               mode = "x", desc = "Toggle list cycle",            ft = markdown_ft },
+        { "<localleader>n",  function() toggle.olist() end,                    mode = "x", desc = "Toggle ordered list",          ft = markdown_ft },
+        { "<localleader>x",  function() toggle.checkbox() end,                 mode = "x", desc = "Toggle checkbox",              ft = markdown_ft },
+        { "<localleader>c",  function() toggle.checkbox_cycle() end,           mode = "x", desc = "Toggle checkbox cycle",        ft = markdown_ft },
+        { "<localleader>h",  function() toggle.heading() end,                  mode = "x", desc = "Toggle heading",               ft = markdown_ft },
 
         -- Auto-list mappings
-        { mode = "n", "O",               function() toggle.autolist_up() end,              desc = "Add list item above",          ft = markdown_ft },
-        { mode = "n", "o",               function() toggle.autolist_down() end,            desc = "Add list item below",          ft = markdown_ft },
-        { mode = "i", "<CR>",            function() toggle.autolist_cr() end,              desc = "Continue list item",           ft = markdown_ft },
+        { "O",               function() toggle.autolist_up() end,              mode = "n", desc = "Add list item above",          ft = markdown_ft },
+        { "o",               function() toggle.autolist_down() end,            mode = "n", desc = "Add list item below",          ft = markdown_ft },
+        { "<CR>",            function() toggle.autolist_cr() end,              mode = "i", desc = "Continue list item",           ft = markdown_ft },
         -- stylua: ignore end
       }
     end,
