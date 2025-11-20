28708507d5cce4338715b297dddb2293a838a659
Zevan770 <151506788+hw770@users.noreply.github.com>
2025-11-20 20:28:43 +0800

    disable block.nvim

--- lua/plugins/ui/block.lua
+++ lua/plugins/ui/block.lua
@@ -1,18 +1,21 @@
+if true then return {} end
+---@type LazySpec
 return {
   {
     "HampusHauffman/block.nvim",
+    -- branch = "re",
     config = function()
-      require("block").setup({
-        automatic = true,
-      })
-    end
+      require("block").setup {
+        -- automatic = true,
+      }
+    end,
   },
   {
     "folke/snacks.nvim",
     opts = {
       indent = {
-        enabled = false
-      }
-    }
-  }
+        enabled = false,
+      },
+    },
+  },
 }
