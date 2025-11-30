ffe220d6c97c273cdc91ca8c9f92b8ed6285e479
Zevan770 <151506788+hw770@users.noreply.github.com>
2025-12-01 02:49:47 +0800

    wip: fix json_ls yamlls schemastore

---
+++ lsp/json_ls.lua
@@ -0,0 +1,26 @@
+---@diagnostic disable: missing-fields, inject-field
+
+---@type vim.lsp.ClientConfig
+return {
+  settings = {
+    json = {
+      validate = {
+        enable = true,
+      },
+    },
+  },
+  before_init = function(_, config)
+    -- can't assign new table because of
+    -- https://github.com/neovim/neovim/issues/27740#issuecomment-1978629315
+    config.settings.json.schemas = require("schemastore").json.schemas()
+  end,
+}
+
+-- vim.lsp.config("jsonls", {
+--     settings = {
+--         json = {
+--             schemas = require("schemastore").json.schemas(),
+--             validate = { enable = true },
+--         },
+--     },
+-- })
