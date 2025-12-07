0413a335452c513b9adc2d854e1a98f9ec8e41b7
Zevan770 <151506788+hw770@users.noreply.github.com>
2025-12-08 02:23:05 +0800

    autocorrect works now

--- lsp/autocorrect.lua
+++ lsp/autocorrect.lua
@@ -1,7 +1,5 @@
 return {
   cmd = { "autocorrect", "server" },
-  -- HACK: 强制使用脚本去掉第一行的启动消息, https://github.com/huacnlee/autocorrect/issues/287
-  -- cmd = { vim.fn.stdpath "config" .. "/lsp/autocorrect-lsp" },
   filetypes = {
     "markdown",
     "text",
