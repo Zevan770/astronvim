ecbdcf77d4384330a76cf0da32e1448138ac2634
Zevan770 <151506788+hw770@users.noreply.github.com>
2026-01-07 06:55:39 +0800

    osc7

--- lua/plugins/other/astrocore.lua
+++ lua/plugins/other/astrocore.lua
@@ -90,6 +90,26 @@ return {
             end,
           },
         },
+
+        term_handle_osc7 = {
+          {
+            event = { "TermRequest" },
+            desc = "Handles OSC 7 dir change requests",
+            callback = function(ev)
+              local val, n = string.gsub(ev.data.sequence, "\027]7;file://[^/]*", "")
+              if n > 0 then
+                -- OSC 7: dir-change
+                local dir = val
+                if vim.fn.isdirectory(dir) == 0 then
+                  vim.notify("invalid dir: " .. dir)
+                  return
+                end
+                vim.b[ev.buf].osc7_dir = dir
+                if vim.api.nvim_get_current_buf() == ev.buf then vim.cmd.lcd(dir) end
+              end
+            end,
+          },
+        },
       },
       filetypes = {
         extension = {
