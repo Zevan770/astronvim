2e6510e0322925deaef5e0a1103971073439e84b
Zevan770 <151506788+hw770@users.noreply.github.com>
2026-02-28 05:04:34 +0800

    fix json yaml

--- lua/plugins/languages/markup.lua
+++ lua/plugins/languages/markup.lua
@@ -1,3 +1,32 @@
+vim.lsp.config("jsonls", {
+  settings = {
+    json = {
+      validate = {
+        enable = true,
+      },
+    },
+  },
+  before_init = function(_, config)
+    -- can't assign new table because of
+    -- https://github.com/neovim/neovim/issues/27740#issuecomment-1978629315
+    config.settings.json.schemas = require("schemastore").json.schemas()
+  end,
+})
+vim.lsp.config("yamlls", {
+  settings = {
+    yaml = {
+      schemaStore = {
+        enable = false,
+        url = "",
+      },
+    },
+  },
+  before_init = function(_, config)
+    -- can't assign new table because of
+    -- https://github.com/neovim/neovim/issues/27740#issuecomment-1978629315
+    config.settings.yaml.schemas = require("schemastore").yaml.schemas()
+  end,
+})
 return {
   { import = "astrocommunity.pack.json" },
 
