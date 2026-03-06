a8b7a200addbb85a60ff52992137e2d38b1a0b70
Zevan770 <151506788+hw770@users.noreply.github.com>
2026-03-07 04:32:29 +0800

    update

--- lua/plugins/languages/python.lua
+++ lua/plugins/languages/python.lua
@@ -1,4 +1,6 @@
 if vim.fn.has "win32" == 1 then return {} end
 return {
-  { import = "astrocommunity.pack.python-ruff" },
+  { import = "astrocommunity.pack.python.base" },
+  { import = "astrocommunity.pack.python.basedpyright" },
+  { import = "astrocommunity.pack.python.ruff" },
 }
