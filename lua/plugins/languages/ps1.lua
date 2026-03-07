1563ade2a480959f3c15ec8a56faf22e0abe537d
Zevan770 <151506788+hw770@users.noreply.github.com>
2026-03-08 03:43:15 +0800

    fix fucking blink lazy loading

--- lua/plugins/languages/ps1.lua
+++ lua/plugins/languages/ps1.lua
@@ -3,20 +3,11 @@ if vim.fn.has "unix" == 1 then return {} end
 return {
   { import = "astrocommunity.pack.ps1" },
   {
-    "AstroNvim/astrolsp",
-    ---@type AstroLSPOpts
-    opts = {
-      servers = {
-        -- "powershell_es",
-      },
-    },
-  },
-  {
     "TheLeoP/powershell.nvim",
-    -- ft = "ps1",
+    ft = "ps1",
     ---@type powershell.user_config
     opts = {
-      capabilities = require("blink.cmp").get_lsp_capabilities(nil, true),
+      -- capabilities = require("blink.cmp").get_lsp_capabilities(nil, true),
       bundle_path = vim.fn.stdpath "data" .. "/mason/packages/powershell-editor-services",
       init_options = {
         enableProfileLoading = false,
