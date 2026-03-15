80362dcac33a22ee38c8e9bc4ba3195837400152
Zevan770 <151506788+hw770@users.noreply.github.com>
2026-03-15 20:39:21 +0800

    vimtex lazy loading

--- lua/plugins/languages/tex.lua
+++ lua/plugins/languages/tex.lua
@@ -3,6 +3,8 @@ return {
   { import = "astrocommunity.markdown-and-latex.vimtex" },
   {
     "lervag/vimtex",
+    lazy = true,
+    ft = { "tex" },
     keys = {
       {
         "<localleader>lt",
