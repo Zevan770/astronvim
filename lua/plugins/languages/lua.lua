a0d44f2590e8d3677912a1eb7b2d8de78db41fbd
Zevan770 <151506788+hw770@users.noreply.github.com>
2026-03-28 17:08:48 +0800

    tweak lazydev

--- lua/plugins/languages/lua.lua
+++ lua/plugins/languages/lua.lua
@@ -1,10 +1,17 @@
 -- if vim.fn.has "android" then return {} end
+vim.lsp.config("lua_ls", {
+  settings = {
+    Lua = {
+      hint = {
+        -- semicolon = "All",
+      },
+    },
+  },
+  single_file_support = true,
+})
 ---@type LazySpec
 return {
-  {
-    "AstroNvim/astrocommunity",
-    { import = "astrocommunity.pack.lua" },
-  },
+  { import = "astrocommunity.pack.lua" },
   {
     "folke/lazydev.nvim",
     dependencies = {
@@ -18,7 +25,7 @@ return {
         -- Only load luvit types when the `vim.uv` word is found
         -- { path = "luvit-meta/library", words = { "vim%.uv" } },
         { path = "snacks.nvim", words = { "Snacks" } },
-        { path = "nvim-lspconfig", words = { "lspconfig" } },
+        { path = "nvim-lspconfig" },
         -- Needs `justinsgithub/wezterm-types` to be installed
         { path = "wezterm-types", mods = { "wezterm" } },
         { path = "neotest", mods = { "neotest" } },
