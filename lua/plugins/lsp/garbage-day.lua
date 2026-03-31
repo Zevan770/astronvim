4a488367f2633283fadef3bb64a961021951891f
Zevan770 <151506788+hw770@users.noreply.github.com>
2026-04-01 01:10:31 +0800

    add garbage-day, lsp server gc

---
+++ lua/plugins/lsp/garbage-day.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    -- Garbage collector that stops inactive LSP clients to free RAM
+    "zeioth/garbage-day.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- your options here
+    },
+  },
+}
