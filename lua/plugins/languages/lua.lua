83322b088a37a07e93722b39d0c96dc864b0a3d8
Zevan770 <151506788+hw770@users.noreply.github.com>
2026-04-13 20:22:35 +0800

    lua_ls reuse_client

--- lua/plugins/languages/lua.lua
+++ lua/plugins/languages/lua.lua
@@ -1,14 +1,4 @@
 -- if vim.fn.has "android" then return {} end
-vim.lsp.config("lua_ls", {
-  settings = {
-    Lua = {
-      hint = {
-        -- semicolon = "All",
-      },
-    },
-  },
-  single_file_support = true,
-})
 ---@type LazySpec
 return {
   { import = "astrocommunity.pack.lua" },
@@ -18,6 +8,17 @@ return {
     opts = function(_, opts)
       opts.ensure_installed = vim.tbl_filter(function(v) return v ~= "lua-language-server" end, opts.ensure_installed)
     end,
+  },
+  {
+    "astronvim/astrolsp",
+    ---@type AstroLSPOpts
+    opts = {
+      config = {
+        lua_ls = {
+          reuse_client = function(client, config) return true end,
+        },
+      },
+    },
   },
   {
     "folke/lazydev.nvim",
@@ -31,11 +32,11 @@ return {
         -- It can also be a table with trigger words / mods
         -- Only load luvit types when the `vim.uv` word is found
         -- { path = "luvit-meta/library", words = { "vim%.uv" } },
-        { path = "snacks.nvim", words = { "Snacks" } },
+        { path = "snacks.nvim",   words = { "Snacks" } },
         { path = "nvim-lspconfig" },
         -- Needs `justinsgithub/wezterm-types` to be installed
         { path = "wezterm-types", mods = { "wezterm" } },
-        { path = "neotest", mods = { "neotest" } },
+        { path = "neotest",       mods = { "neotest" } },
         -- "lazy.nvim",
       },
     },
