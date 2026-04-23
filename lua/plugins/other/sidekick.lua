a2984bada9a35d5306e479a4baf4114e6ec1b5c2
Zevan770 <151506788+hw770@users.noreply.github.com>
2026-04-24 05:39:08 +0800

    fix sidekick

--- lua/plugins/other/sidekick.lua
+++ lua/plugins/other/sidekick.lua
@@ -27,8 +27,8 @@ return {
             return -- jumped or applied
           end
 
-          -- if you are using Neovim's native inline completions
-          if vim.lsp.inline_completion.get() then return end
+          -- -- if you are using Neovim's native inline completions
+          -- if vim.lsp.inline_completion.get() then return end
 
           -- any other things (like snippets) you want to do on <tab> go here.
 
