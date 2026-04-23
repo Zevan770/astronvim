904f827a898292f199cbbf669071d96640675b37
Zevan770 <151506788+hw770@users.noreply.github.com>
2026-04-24 05:37:40 +0800

    use ty instead of ruff

--- lua/plugins/languages/python.lua
+++ lua/plugins/languages/python.lua
@@ -3,6 +3,6 @@ vim.lsp.enable("ruff")
 return {
   { import = "astrocommunity.pack.python.base" },
   { import = "astrocommunity.pack.python.basedpyright" },
-  -- { import = "astrocommunity.pack.python.ty" },
-  { import = "astrocommunity.pack.python.ruff" },
+  { import = "astrocommunity.pack.python.ty" },
+  -- { import = "astrocommunity.pack.python.ruff" },
 }
