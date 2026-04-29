688ae5fea191dcc7a65c64e3db98a4a2f9b304e7
Zevan770 <151506788+hw770@users.noreply.github.com>
2026-04-30 01:48:48 +0800

    enable tiny-inline-diagnostic

--- lua/plugins/lsp/tiny-inline-diagnostic.lua
+++ lua/plugins/lsp/tiny-inline-diagnostic.lua
@@ -2,10 +2,18 @@
 return {
   "rachartier/tiny-inline-diagnostic.nvim",
   event = "LspAttach",
-  enabled = false,
   priority = 1000, -- needs to be loaded in first
   config = function()
-    require("tiny-inline-diagnostic").setup()
     vim.diagnostic.config { virtual_text = false } -- Only if needed in your configuration, if you already have native LSP diagnostics
+    require("tiny-inline-diagnostic").setup {
+      transparent_bg = false,
+      options = {
+        multilines = { enabled = true, always_show = false },
+        virt_texts = { priority = 2048 },
+      },
+      disabled_ft = {},
+    }
+
+    vim.diagnostic.open_float = require("tiny-inline-diagnostic.override").open_float
   end,
 }
