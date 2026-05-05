6ce3b3052b1f11d1a45351459ac23e0fb0f0dc44
Zevan770 <151506788+hw770@users.noreply.github.com>
2026-05-05 22:43:07 +0800

    让 lsp 的 string 透明，从而优先使用 treesitter 的 hl_group

--- lua/plugins/ui/astroui.lua
+++ lua/plugins/ui/astroui.lua
@@ -1,4 +1,7 @@
 ---@type LazySpec
+-- https://sitr.us/2026/05/03/embedded-sql-highlighting-in-neovim/#:~:text=The%20way%20I%20did%20that%20was%20to%20set%20that%20highlight%20to%20be%20transparent
+-- 让 lsp 的 string 透明，从而优先使用 treesitter 的 hl_group
+vim.api.nvim_set_hl(0, "@lsp.type.string", {})
 return {
   "AstroNvim/astroui",
   ---@type AstroUIOpts
