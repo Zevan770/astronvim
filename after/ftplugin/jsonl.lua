1cd5c115e51c8515794ea6fcb3f566c5ee86af79
Zevan770 <151506788+hw770@users.noreply.github.com>
2026-05-09 16:15:57 +0800

    jsonl

--- after/ftplugin/jsonl.lua
+++ after/ftplugin/jsonl.lua
@@ -1,5 +1,16 @@
-vim.keymap.set("n", ';<space>', function()
-  require("toggleterm").exec("fx -s " .. vim.api.nvim_buf_get_name(0) .. " | fx")
-end)
+vim.keymap.set(
+  "n",
+  "<localleader><space>",
+  function() require("toggleterm").exec("fx -s " .. vim.api.nvim_buf_get_name(0) .. " | fx") end,
+  { buffer = true }
+)
+
+vim.keymap.set("n", "<localleader>x", function()
+  local line = vim.api.nvim_get_current_line()
+  require("toggleterm").exec("echo " .. vim.fn.shellescape(line) .. " | fx")
+end, {
+  desc = "Send current line to fx",
+  buffer = true,
+})
 
 vim.treesitter.language.register("json", "jsonl")
