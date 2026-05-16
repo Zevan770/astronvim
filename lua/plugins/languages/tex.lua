fe743a55f069ef0922874f675b4eb3d90a63ab16
Zevan770 <151506788+hw770@users.noreply.github.com>
2026-05-16 09:09:34 +0800

    tweak vimtex

--- lua/plugins/languages/tex.lua
+++ lua/plugins/languages/tex.lua
@@ -5,6 +5,24 @@ return {
     "lervag/vimtex",
     lazy = true,
     ft = { "tex" },
+    init = function(_)
+      -- vim.g.vimtex_view_method = "general"
+      -- vim.g.vimtex_view_general_viewer = "sumatrapdf"
+      -- vim.g.vimtex_view_general_options = "-reuse-instance -forward-search @tex @line @pdf"
+      -- vim.cmd [[
+      --   if has('win32') || (has('unix') && exists('$WSLENV'))
+      --     if executable('sioyek.exe')
+      --       let g:vimtex_view_method = 'sioyek'
+      --       let g:vimtex_view_sioyek_exe = 'sioyek.exe'
+      --       let g:vimtex_callback_progpath = 'wsl nvim'
+      --     elseif executable('mupdf.exe')
+      --       let g:vimtex_view_general_viewer = 'mupdf.exe'
+      --     elseif executable('SumatraPDF.exe')
+      --       let g:vimtex_view_general_viewer = 'SumatraPDF.exe'
+      --     endif
+      --   endif
+      -- ]]
+    end,
     keys = {
       {
         "<localleader>lt",
