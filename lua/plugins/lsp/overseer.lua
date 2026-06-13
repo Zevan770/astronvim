890618229c2dc1b4c767d9eeb66d610d707b4980
Zevan770 <151506788+hw770@users.noreply.github.com>
2026-06-13 17:11:13 +0800

    overseer make

--- lua/plugins/lsp/overseer.lua
+++ lua/plugins/lsp/overseer.lua
@@ -16,14 +16,40 @@ return {
       },
       { "AstroNvim/astroui", opts = { icons = { Overseer = "" } } },
     },
-    ---@module "overseer"
-    ---@type overseer.Config
-    opts = {},
+    opts = function(self, opts)
+      vim.api.nvim_create_user_command("Make", function(params)
+        -- Insert args at the '$*' in the makeprg
+        local cmd, num_subs = vim.o.makeprg:gsub("%$%*", params.args)
+        if num_subs == 0 then cmd = cmd .. " " .. params.args end
+        local task = require("overseer").new_task {
+          cmd = vim.fn.expandcmd(cmd),
+          components = {
+            {
+              "on_output_quickfix",
+              open = not params.bang,
+              open_height = 8,
+              errorformat = vim.o.errorformat,
+            },
+            "default",
+          },
+        }
+        task:start()
+      end, {
+        desc = "Run your makeprg as an Overseer task",
+        nargs = "*",
+        bang = true,
+      })
+
+      ---@module "overseer"
+      ---@type overseer.Config
+      return {}
+    end,
     keys = {
       { "<leader>;t", "<Cmd>OverseerToggle<CR>" },
-      { "<leader>;;", "<Cmd>OverseerShell<CR>" },
-      { "<leader>;r", "<Cmd>OverseerRun<CR>" },
+      { "<leader>;;", ":OverseerShell " },
+      { "<leader>;f", "<Cmd>OverseerRun<CR>" },
       { "<leader>;a", "<Cmd>OverseerTaskAction<CR>" },
+      { "<leader>;m", "<Cmd>Make<CR>" },
       { "<leader>;i", "<Cmd>checkhealth overseer<CR>" },
     },
   },
