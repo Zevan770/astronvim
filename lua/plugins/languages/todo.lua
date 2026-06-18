4adba6f8e92002beb9f4c7e5751936bdfa26e0dc
Zevan770 <151506788+hw770@users.noreply.github.com>
2026-06-18 14:09:20 +0800

    add todotxt.nvim

--- lua/plugins/languages/todo.lua
+++ lua/plugins/languages/todo.lua
@@ -1,5 +1,33 @@
 return {
+  -- {
+  --   "freitass/todo.txt-vim",
+  -- },
   {
-    "freitass/todo.txt-vim",
+    "phrmendes/todotxt.nvim",
+    init = function()
+      vim.filetype.add {
+        filename = {
+          ["todo.txt"] = "todotxt",
+          ["done.txt"] = "todotxt",
+        },
+      }
+    end,
+    opts = {
+      todotxt = vim.env.HOME .. "/todo.txt",
+      donetxt = vim.env.HOME .. "/done.txt",
+      max_priority = "C",
+      metadata = {
+        tag = { sort = "asc" },
+        due = { sort = "asc" },
+      },
+      ghost_text = {
+        enable = true,
+        mappings = {
+          ["(A)"] = "today",
+          ["(B)"] = "tomorrow",
+          ["(C)"] = "this week",
+        },
+      },
+    },
   },
 }
