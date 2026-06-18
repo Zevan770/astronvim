fabffe046a3d97e376e71809a441e090527d4bc8
Zevan770 <151506788+hw770@users.noreply.github.com>
2026-06-19 01:41:06 +0800

    add bufix

---
+++ lua/plugins/lsp/bufix.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "msaher/bufix.nvim",
+    opts = function()
+      local group = vim.api.nvim_create_augroup("BufixTerm", { clear = true })
+      vim.api.nvim_create_autocmd("TermOpen", {
+        group = group,
+        callback = function(opts)
+          require("bufix.api").register_buf(opts.buf) -- make it work with goto_next() and friends
+        end,
+      })
+      return {}
+    end,
+  },
+}
