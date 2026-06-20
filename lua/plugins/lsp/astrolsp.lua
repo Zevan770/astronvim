a854971e6e06c6f99c3595c3c79dfc145ef133fe
Zevan770 <151506788+hw770@users.noreply.github.com>
2026-06-21 03:33:53 +0800

    config

--- lua/plugins/lsp/astrolsp.lua
+++ lua/plugins/lsp/astrolsp.lua
@@ -1,9 +1,9 @@
--- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+vim.lsp.config("*", {
+  reuse_client = function(client, config) return client.name == config.name end,
+})
 
 -- AstroLSP allows you to customize the features in AstroNvim's LSP configuration engine
 -- Configuration documentation can be found with `:h astrolsp`
--- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
---       as this provides autocomplete and documentation while editing
 ---@type LazySpec
 return {
   "AstroNvim/astrolsp",
