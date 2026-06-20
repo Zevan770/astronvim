1b0d36189b597f6731ffe1205011edfbe6d167b3
Zevan770 <151506788+hw770@users.noreply.github.com>
2026-06-20 15:52:02 +0800

    enable trouble

--- lua/plugins/lsp/trouble.lua
+++ lua/plugins/lsp/trouble.lua
@@ -1,4 +1,4 @@
-if true then return {} end
+-- if true then return {} end
 ---@type LazySpec
 return {
   { import = "astrocommunity.diagnostics.trouble-nvim" },
@@ -20,7 +20,9 @@ return {
   {
     "folke/trouble.nvim",
     opts = {
-      auto_open = false, -- auto open when there are items
+      modes = {
+        -- diagnostics = { auto_open = true },
+      },
     },
     keys = { { "<leader>lx", "<cmd>Trouble lsp toggle<CR>", mode = { "n", "v" }, desc = "Trouble lsp" } },
   },
