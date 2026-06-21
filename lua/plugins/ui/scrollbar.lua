407e7fbb60707266dfa8d14cb9ee424087c28b14
Zevan770 <151506788+hw770@users.noreply.github.com>
2026-06-22 03:20:26 +0800

    add satallite as crollbar

--- lua/plugins/ui/scrollbar.lua
+++ lua/plugins/ui/scrollbar.lua
@@ -1,14 +1,32 @@
 ---@type LazySpec
 return {
-  "petertriho/nvim-scrollbar",
-  opts = function(_, opts)
-    require("astrocore").extend_tbl(opts, {
+  {
+    "petertriho/nvim-scrollbar",
+    enabled = false,
+    opts = function(_, opts)
+      require("astrocore").extend_tbl(opts, {
+        handlers = {
+          gitsigns = require("astrocore").is_available "gitsigns.nvim",
+          search = require("astrocore").is_available "nvim-hlslens",
+          ale = require("astrocore").is_available "ale",
+        },
+      })
+    end,
+    event = "User AstroFile",
+  },
+  {
+    -- scrollbar
+    "lewis6991/satellite.nvim",
+    event = "User AstroFile",
+    opts = {
+      excluded_filetypes = { "prompt", "TelescopePrompt", "noice", "notify", "neo-tree" },
       handlers = {
-        gitsigns = require("astrocore").is_available "gitsigns.nvim",
-        search = require("astrocore").is_available "nvim-hlslens",
-        ale = require("astrocore").is_available "ale",
+        marks = {
+          enable = true,
+          show_builtins = false, -- shows the builtin marks like [ ] < >
+          key = "M",
+        },
       },
-    })
-  end,
-  event = "User AstroFile",
+    },
+  },
 }
