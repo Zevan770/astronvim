55e89f2e471c21516cb4ee3402dbe945b12237a7
Zevan770 <151506788+hw770@users.noreply.github.com>
2026-07-15 02:11:27 +0800

    cleanup nixd config and enable codesettings

--- lua/os/nixos/nix.lua
+++ lua/os/nixos/nix.lua
@@ -1,38 +1,4 @@
 ---@type LazySpec
 return {
   { import = "astrocommunity.pack.nix" },
-  {
-    "AstroNvim/astrolsp",
-    ---@param opts AstroLSPOpts
-    opts = function(self, opts)
-      opts.servers = opts.servers or {}
-      -- opts.servers = vim.tbl_filter(function(server) return server ~= "nixd" end, opts.servers)
-      -- require("astrocore").list_insert_unique(opts.servers, { "tix" })
-      return require("lazy.util").merge(opts, {
-        config = {
-          nixd = {
-            reuse_client = function(client, config) return client.name == config.name end,
-            settings = {
-              nixd = {
-                nixpkgs = {
-                  expr = "import <nixpkgs> { }",
-                },
-                options = {
-                  nixos = {
-                    expr = '(builtins.getFlake ("git+file://" + toString ./.)).nixosConfigurations.nixos.options',
-                  },
-                  home_manager = {
-                    expr = "(builtins.getFlake (builtins.toString ./.)).nixosConfigurations.nixos.options.home-manager.users.type.getSubOptions []",
-                  },
-                },
-                formatting = {
-                  command = { "nixfmt" },
-                },
-              },
-            },
-          },
-        },
-      })
-    end,
-  },
 }
--- lua/plugins/lsp/codesettings.lua
+++ lua/plugins/lsp/codesettings.lua
@@ -1,8 +1,10 @@
+---@type LazySpec
 return {
   {
     "mrjones2014/codesettings.nvim",
-    enabled = false,
+    -- enabled = false,
     -- these are the default settings just set `opts = {}` to use defaults
+    lazy = false,
     opts = {
       ---Look for these config files
       config_file_paths = { ".vscode/settings.json", "codesettings.json", "lspsettings.json" },
@@ -11,9 +13,37 @@ return {
       ---Set filetype to jsonc when opening a file specified by `config_file_paths`,
       ---make sure you have the jsonc tree-sitter parser installed for highlighting
       jsonc_filetype = true,
-      lua_ls_integration = false,
+      lua_ls_integration = true,
+      live_reload = true,
     },
-    ft = { "json", "jsonc", "lua" },
+    init = function()
+      vim.lsp.config("*", {
+        before_init = function(_, config)
+          local codesettings = require "codesettings"
+          -- Chainsaw(config)
+          codesettings.with_local_settings(config.name, config)
+          -- Chainsaw(config)
+        end,
+      })
+    end,
     -- event = "VeryLazy",
   },
+
+  -- {
+  --   "AstroNvim/astrolsp",
+  --   ---@type AstroLspOpts
+  --   opts = {
+  --     config = {
+  --       ["*"] = {
+  --         before_init = function(_, config)
+  --           local codesettings = require "codesettings"
+  --           vim.notify(config.name .. " before_init")
+  --           Chainsaw(config)
+  --           codesettings.with_local_settings(config.name, config)
+  --           Chainsaw(config)
+  --         end,
+  --       },
+  --     },
+  --   },
+  -- },
 }
